52063e2dc47f7a4896d1dd12294aea1240a704b3
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:35:20 +0100

    Add project files.

---
+++ OpenLab 02.8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OpenLab_02._8
+{
+    class Program
+    {
+        public static bool IsEmpty(string a)
+        {
+            return String.IsNullOrEmpty(a);
+        }
+        static void Main(string[] args)
+        {
+            string a = "a";
+            Console.WriteLine(IsEmpty(a));
+        }
+    }
+}
